45b0513707c60bcf87ab0a8e21c019f7f2b39461
Bogdan <maksimukmaks1707@gmail.com>
2023-08-02 19:49:26 +0300

    add new file

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN", "5579393510:AAFTu9JVaAd9IyujIkX3wN7NfkCHhJYAxdk")
+TOKEN_ALERT = os.environ.get("TOKEN_ALERT", "45ecc9718df67ffa5b74af05ed641afebc29474fab2203")
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import aiogram
+from create_bot import bot, dp
+
+
+async def on_startup() -> None:
+    print("Bot Online")
+
+
+async def start_bot():
+    await on_startup()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
